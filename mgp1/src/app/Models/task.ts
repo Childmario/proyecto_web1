613e0580c20fedf0f6bf0b8471494704a1835479
childmario2 <mrob127@yahoo.es>
2018-09-16 15:48:39 -0600

    Creacion de modelo
    Creacion de modelo para intefaz de comentarios

---
+++ mgp1/src/app/Models/task.ts
@@ -0,0 +1,5 @@
+interface Task{
+    nombre: string;
+    correo: string;
+    mensaje: string;
+}
--- mgp1/src/app/app.module.ts
+++ mgp1/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { HolaMundo } from "./primercomponent/primer.component";
 import { TestComponentComponent } from './test-component/test-component.component';
 import { UserComponent } from './user/user.component';
 import {RouterModule, Routes} from '@angular/router';
+import { DataService } from "./servicios/data.service";
 
 @NgModule({
   declarations: [
@@ -16,12 +18,15 @@ import {RouterModule, Routes} from '@angular/router';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot([
 {path: 'crear', component: UserComponent},
 {path: 'visualizar', component: TestComponentComponent}
     ])
   ],
-  providers: [],
+  providers: [
+DataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
